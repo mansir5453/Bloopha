67b932da0dc0dc19db7ad12f1c102fedaf943a5b
aditya singh <you@example.com>
2025-12-03 23:23:43 +0530

    Cleanup unused components and sections

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,9 @@ import { HeroSection } from "@/components/sections/HeroSection";
 import { ScrollTextSection } from "@/components/sections/ScrollTextSection";
 import { WavyTextSection } from "@/components/sections/WavyTextSection";
 import { MixedGallerySection } from "@/components/sections/MixedGallerySection";
-import { OurWorkSection } from "@/components/sections/OurWorkSection";
 import { ExpandedServicesSection } from "@/components/sections/ExpandedServicesSection";
 import { TechStackSection } from "@/components/sections/TechStackSection";
 import { JourneySection } from "@/components/sections/JourneySection";
-import { PortfolioSection } from "@/components/sections/PortfolioSection";
 import { ScrollExpansionSection } from "@/components/sections/ScrollExpansionSection";
 import { ContactSection } from "@/components/sections/ContactSection";
 import { Footer } from "@/components/ui/footer-section";
@@ -32,12 +30,10 @@ export default function Home() {
           <ScrollTextSection />
           <WavyTextSection />
           <MixedGallerySection />
-          {/* <OurWorkSection /> */}
           <ExpandedServicesSection />
           <TechStackSection />
           <JourneySection />
           <ScrollExpansionSection />
-          {/* <PortfolioSection /> */}
           <ContactSection />
         </div>
       </AuroraBackground>
